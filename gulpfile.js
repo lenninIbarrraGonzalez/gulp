bc1ff0e29954416b5af0ee79cc977189e4038c82
Lennin Ibarra <ing.lenninibarra@gmail.com>
2021-07-22 15:24:17 -0500

    leyendo un archivo y grabandolo en una ruta, procesando archivos con .src .dest y .pipe

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
 
-gulp.task('hola', function () {
-  console.log('Hola');
-});
+// gulp.task('hola', function () {
+//   console.log('Hola');
+// });
 
-gulp.task('hola', function (cb) {
-  setTimeout(function () {
-    console.log('Hola');
-    cb();
-  }, 5000);
+// gulp.task('hola', function (cb) {
+//   setTimeout(function () {
+//     console.log('Hola');
+//     cb();
+//   }, 5000);
 
-})
+// })
 
 //En la version 3 para esperar que se ejecute primer una funcion antes que la otra
 
@@ -18,12 +18,20 @@ gulp.task('hola', function (cb) {
 //   console.log('Mundo');
 // });
 
-gulp.task('mundo', function () {
-  console.log('Mundo');
-});
+// gulp.task('mundo', function () {
+//   console.log('Mundo');
+// });
 
 //en la version 3
 // gulp.task('default', ['hola', 'mundo']);
 
 //En la version 4
-gulp.task('default', gulp.series('hola', 'mundo'));
+
+gulp.task('sass', function () {
+  return gulp.src('src/style.sass')
+    .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('default', ['sass']);
+
+
