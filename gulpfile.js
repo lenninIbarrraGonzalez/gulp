678f91485f1084b0a64fd7d17edac85d4cb8751a
Lennin Ibarra <ing.lenninibarra@gmail.com>
2021-07-22 12:12:27 -0500

    Creando funciones asincronas

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,23 @@ gulp.task('hola', function () {
   console.log('Hola');
 });
 
+gulp.task('hola', function (cb) {
+  setTimeout(function () {
+    console.log('Hola');
+    cb();
+  }, 5000);
+
+})
+
+//En la version 3 para esperar que se ejecute primer una funcion antes que la otra
+
+// gulp.task('mundo', ['hola'], function () {
+//   console.log('Mundo');
+// });
+
 gulp.task('mundo', function () {
   console.log('Mundo');
-})
+});
 
 //en la version 3
 // gulp.task('default', ['hola', 'mundo']);
